0f63c43ceb12602b11ed7432cca61f9529d5c839
skyfoojs <skyfoojs@gmail.com>
2025-10-15 17:19:23 +0800

    Add auth validatior

--- root/backend/app/validators/auth/auth-validator.ts
+++ root/backend/app/validators/auth/auth-validator.ts
@@ -4,14 +4,18 @@
  * Note: Only will be used for APIs that contains request body
  */
 import { body } from "express-validator";
-import validate from "../middlewares/validate";
+import validate from "../../middlewares/validate";
+import { ENUM_USER_ROLE } from "../../enums/enums";
 
 export const login: any = [
-  body('identifier')
-    .notEmpty().withMessage('Missing identifier')
-    .isString().withMessage('Indentifier must be string'),
+  body('userId')
+    .notEmpty().withMessage('Missing userId')
+    .isNumeric().withMessage('Indentifier must be number'),
   body('password')
     .notEmpty().withMessage('Missing password')
     .isString().withMessage('Password must be string'),
+  body('role')
+    .notEmpty().withMessage('Missing role')
+    .isIn(Object.values(ENUM_USER_ROLE)).withMessage('Role must user or admin'),
   validate,
 ]
