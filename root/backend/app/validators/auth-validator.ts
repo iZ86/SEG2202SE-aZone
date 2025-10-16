68112ff3b1f1b98d3b7f50eadfd98ca5eb8883a2
skyfoojs <skyfoojs@gmail.com>
2025-10-16 16:16:11 +0800

    Update auth validator

---
+++ root/backend/app/validators/auth-validator.ts
@@ -0,0 +1,30 @@
+import { body } from "express-validator";
+import validate from "../middlewares/validate";
+import { ENUM_USER_ROLE } from "../enums/enums";
+
+export const loginBodyValidator: any = [
+  body('userId')
+    .notEmpty().withMessage('Missing userId')
+    .isNumeric().withMessage('Indentifier must be number'),
+  body('password')
+    .trim()
+    .notEmpty().withMessage('Missing password')
+    .isString().withMessage('Password must be string'),
+  body('role')
+    .trim()
+    .notEmpty().withMessage('Missing role')
+    .isIn(Object.values(ENUM_USER_ROLE)).withMessage('Role must user or admin'),
+  validate,
+]
+
+export const updateMeBodyValidator: any = [
+  body('phoneNumber')
+    .trim()
+    .notEmpty().withMessage('Missing phoneNumber')
+    .matches(/^01\d{8,9}$/).withMessage("phoneNumber must start with 01 and contain 10-11 digits"),
+  body('email')
+    .trim()
+    .notEmpty().withMessage('Missing email')
+    .isEmail().withMessage('email must be email'),
+  validate,
+]
