e0e91c4ae1533401f3325a700a2080f7223abf39
skyfoojs <skyfoojs@gmail.com>
2025-10-16 16:15:56 +0800

    Remove comment

--- root/backend/app/models/user-model.ts
+++ root/backend/app/models/user-model.ts
@@ -1,8 +1,3 @@
-
-/**
- * Models folder will be mainly used for data types
- * This is an example for User Model
- * */
 import { RowDataPacket } from "mysql2";
 
 export interface UserData extends RowDataPacket {
