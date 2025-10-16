9c05a8ae9a30ceddf7a6812896cac8ada8669727
iZ86 <iZLikesCS@gmail.com>
2025-10-16 17:18:00 +0800

    Update route to using route aliasing

--- root/frontend/src/app/router.tsx
+++ root/frontend/src/app/router.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import GuestLogin from "./routes/GuestLogin";
+import GuestLogin from "@routes/GuestLogin";
 
 const routes = [
   {
