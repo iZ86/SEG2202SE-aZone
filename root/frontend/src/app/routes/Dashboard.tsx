1a150544af12a3f00ec05d9e3d94fcc0b708933f
iZ86 <iZLikesCS@gmail.com>
2025-10-18 16:36:51 +0800

    Update StudentNavbar import and add bg-white-antiflash to page

--- root/frontend/src/app/routes/Dashboard.tsx
+++ root/frontend/src/app/routes/Dashboard.tsx
@@ -4,12 +4,12 @@
 import SubjectInformationPanel from "@features/subjects/components/SubjectInformationPanel";
 import StudentInformationPanel from "@features/student/components/StudentInformationPanel";
 import TimetablePanel from "@features/timetable/components/TimetablePanel";
-import StudentNavbar from "@components/StudentNavbar";
+import StudentNavbar from "@components/student/StudentNavbar";
 
 /** TBD: A042 */
 export default function Dashboard() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-white-antiflash">
       <StudentNavbar />
       <div>
         {/** TODO: Fill in this with the appropriate components. */}
