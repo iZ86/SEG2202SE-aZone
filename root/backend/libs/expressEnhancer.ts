62a944fd208157eb4931e725fa7394f1aaa7dd73
skyfoojs <skyfoojs@gmail.com>
2025-10-19 10:20:34 +0800

    Add .ok(), .create(), and .delete() for sendSuccess response

--- root/backend/libs/expressEnhancer.ts
+++ root/backend/libs/expressEnhancer.ts
@@ -17,7 +17,12 @@ declare module 'express-serve-static-core' {
   }
 
   interface Response {
-    sendSuccess<T>(data: T, message?: string): void;
+    sendSuccess: {
+      ok(data: any, message?: string): void;
+      create(data: any, message?: string): void;
+      delete(message?: string): void;
+    };
+
     sendError: {
       badRequest(message?: string): void;
       unauthorized(message?: string): void;
@@ -37,45 +42,86 @@ declare module 'express-serve-static-core' {
 export function enhanceResponse(req: Request, res: Response, next: NextFunction) {
   const r = res as Response;
 
-  r.sendSuccess = function <T>(data: T, message = "Success") {
-    return this.status(200).json({
-      success: true,
-      data,
-      message,
-    });
+  r.sendSuccess = {
+    ok(data, message = "Success") {
+      return r.status(200).json({
+        success: true,
+        data,
+        message,
+      });
+    },
+
+    create(data, message = "Created") {
+      return r.status(201).json({
+        success: true,
+        data,
+        message,
+      });
+    },
+
+    delete(message = "Delete") {
+      return r.status(204).json({
+        success: true,
+        message,
+      });
+    },
   };
 
   r.sendError = {
     badRequest(message = "Bad Request") {
-      return r.status(400).json({ success: false, message });
+      return r.status(400).json({
+        success: false,
+        message
+      });
     },
 
     unauthorized(message = "Unauthorized") {
-      return r.status(401).json({ success: false, message });
+      return r.status(401).json({
+        success: false,
+        message
+      });
     },
 
     forbidden(message = "Forbidden") {
-      return r.status(403).json({ success: false, message });
+      return r.status(403).json({
+        success: false,
+        message
+      });
     },
 
     notFound(message = "Not Found") {
-      return r.status(404).json({ success: false, message });
+      return r.status(404).json({
+        success: false,
+        message
+      });
     },
 
     invalidContentType(message = "Invalid Content Type") {
-      return r.status(415).json({ success: false, message });
+      return r.status(415).json({
+        success: false,
+        message
+      });
     },
 
     tooManyRequests(message = "Too Many Requests") {
-      return r.status(429).json({ success: false, message });
+      return r.status(429).json({
+        success: false,
+        message
+      });
     },
 
     internal(message = "Internal Server Error") {
-      return r.status(500).json({ success: false, message });
+      return r.status(500).json({
+        success: false,
+        message
+      });
     },
 
     maintenance(message = "Service Unavailable") {
-      return r.status(503).json({ success: false, message });
+      return r.status(503).json({
+        success: false,
+        message
+      });
     },
   };
 
