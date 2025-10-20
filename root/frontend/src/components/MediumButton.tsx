d29e93d20d32ac6bcf08fb2cc1ce3ebb17866c32
iZ86 <iZLikesCS@gmail.com>
2025-10-20 17:12:15 +0800

    Add MediumButton Component

---
+++ root/frontend/src/components/MediumButton.tsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+
+
+/** Medium button format. */
+export default function MediumButton({
+  buttonText, submit = false, backgroundColor = "bg-blue-air-superiority", hoverBgColor = "hover:bg-blue-yinmn", borderColor = "", textColor = "text-white", link = "", onClick = undefined
+}: {
+  buttonText: string, submit?: boolean, backgroundColor?: string, hoverBgColor?: string, borderColor?: string, textColor?: string, link?: string, onClick?: () => {}
+}) {
+
+  const commonClasses = `px-4 min-h-10 ${backgroundColor} ${textColor} font-semibold text-xl flex justify-center items-center rounded-xl ${hoverBgColor} ${borderColor.length === 0 ? "" : "border-2 " + borderColor} cursor-pointer`
+
+  if (link.length > 0) {
+    return (
+      <Link to={link} className={commonClasses}>{buttonText}</Link>
+    );
+  } else {
+    return (
+      <input type={`${submit ? "submit" : "button"}`} className={commonClasses} value={buttonText} onClick={onClick} />
+    );
+
+  }
+
+}
