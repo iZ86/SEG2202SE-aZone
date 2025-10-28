4ce6cb330fbe6f359d6f5bbef10fd0f8a085f251
skyfoojs <skyfoojs@gmail.com>
2025-10-28 10:52:05 +0800

    Refactor role swapping tab

--- root/frontend/src/app/routes/admin/AdminUser.tsx
+++ root/frontend/src/app/routes/admin/AdminUser.tsx
@@ -1,17 +1,35 @@
 import AdminNavbar from "@components/admin/AdminNavbar";
-import AdminUserTable from "@features/admin/components/AdminUserTable";
+import type { User } from "@datatypes/userType";
+import AdminUserTable from "@features/admin/components/UserTable";
+import { useState } from "react";
 
 export default function AdminUser() {
+  const [activeTab, setActiveTab] = useState<User["role"]>("Student");
   return (
     <div className="min-h-screen bg-slate-100 flex flex-col">
-          {/* Navbar + Sidebar button */}
-          <AdminNavbar page="users"/>
+      {/* Navbar + Sidebar button */}
+      <AdminNavbar page="users" />
 
-          {/* Main Content */}
-          <main className="px-6 py-6 flex-1">
-            <h1 className="text-3xl font-bold text-slate-900">Users</h1>
-            <AdminUserTable />
-          </main>
+      {/* Main Content */}
+      <main className="px-6 py-6 flex-1">
+        <h1 className="text-3xl font-bold text-slate-900">Users</h1>
+        <div className="flex space-x-8 border-b border-gray-300 mt-6">
+          {(["Student", "Admin"] as User["role"][]).map((role) => (
+            <button
+              key={role}
+              className={`pb-2 font-semibold cursor-pointer ${
+                activeTab === role
+                  ? "border-b-2 border-blue-600 text-blue-600"
+                  : "text-gray-500 hover:text-gray-700"
+              }`}
+              onClick={() => setActiveTab(role)}
+            >
+              {role}
+            </button>
+          ))}
         </div>
+        <AdminUserTable activeTab={activeTab} />
+      </main>
+    </div>
   );
 }
