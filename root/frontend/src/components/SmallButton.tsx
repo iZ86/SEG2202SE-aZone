e7ccd635b7a7f433c07768e04a348f40e022e66b
skyfoojs <skyfoojs@gmail.com>
2025-10-29 17:38:11 +0800

    Fix eslint warning, and prettier the code

--- root/frontend/src/components/SmallButton.tsx
+++ root/frontend/src/components/SmallButton.tsx
@@ -1,24 +1,43 @@
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 /** Small button format. */
 export default function SmallButton({
-  buttonText, submit = false, backgroundColor = "bg-blue-air-superiority", hoverBgColor = "hover:bg-blue-yinmn", borderColor = "", textColor = "text-white", link = "", onClick = undefined
+  buttonText,
+  submit = false,
+  backgroundColor = "bg-blue-air-superiority",
+  hoverBgColor = "hover:bg-blue-yinmn",
+  borderColor = "",
+  textColor = "text-white",
+  link = "",
+  onClick = undefined,
 }: {
-  buttonText: string, submit?: boolean, backgroundColor?: string, hoverBgColor?: string, borderColor?: string, textColor?: string, link?: string, onClick?: () => {}
+  buttonText: string;
+  submit?: boolean;
+  backgroundColor?: string;
+  hoverBgColor?: string;
+  borderColor?: string;
+  textColor?: string;
+  link?: string;
+  onClick?: () => void;
 }) {
-
-  const commonClasses = `font-nunito-sans px-4 py-2 ${backgroundColor} ${textColor} font-bold text-base flex justify-center items-center rounded-sm ${hoverBgColor} ${borderColor.length === 0 ? "" : "border-2 " + borderColor} cursor-pointer`
+  const commonClasses = `font-nunito-sans px-4 py-2 ${backgroundColor} ${textColor} font-bold text-base flex justify-center items-center rounded-sm ${hoverBgColor} ${
+    borderColor.length === 0 ? "" : "border-2 " + borderColor
+  } cursor-pointer`;
 
   if (link.length > 0) {
     return (
-      <Link to={link} className={commonClasses}>{buttonText}</Link>
+      <Link to={link} className={commonClasses}>
+        {buttonText}
+      </Link>
     );
   } else {
     return (
-      <input type={`${submit ? "submit" : "button"}`} className={commonClasses} value={buttonText} onClick={onClick} />
+      <input
+        type={`${submit ? "submit" : "button"}`}
+        className={commonClasses}
+        value={buttonText}
+        onClick={onClick}
+      />
     );
-
   }
-
 }
