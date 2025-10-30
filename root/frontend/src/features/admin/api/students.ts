bb2b088c34905a81c9f59164cce31413eb596928
skyfoojs <skyfoojs@gmail.com>
2025-10-30 13:21:03 +0800

    Add APIs for StudentCourseProgrammeIntake

--- root/frontend/src/features/admin/api/students.ts
+++ root/frontend/src/features/admin/api/students.ts
@@ -14,25 +14,9 @@ export const getAllStudentsAPI = async (token: string, pageSize: number, page: n
   }
 };
 
-export const getAllStudentCourseProgrammeIntakesAPI = async (token: string, pageSize: number, page: number, query: string): Promise<Response | undefined> => {
+export const getStudentByIdAPI = async (token: string, studentId: number): Promise<Response | undefined> => {
   try {
-    return await fetch("http://localhost:8080/api/v1/users/students/course/programme/intake?" + (query ? `query=${query}&` : '') + `pageSize=${pageSize}&page=${page}`,
-      {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json'
-        },
-        mode: "cors"
-      });
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-export const getStudentCourseProgrammeIntakeByStudentIdAPI = async (token: string, studentId: number): Promise<Response | undefined> => {
-  try {
-    return await fetch(`http://localhost:8080/api/v1/users/students/${studentId}/course/programme/intake`,
+    return await fetch(`http://localhost:8080/api/v1/users/students/${studentId}`,
       {
         method: 'GET',
         headers: {
@@ -87,6 +71,75 @@ export const updateStudentByIdAPI = async (token: string, studentId: number, fir
           password,
           userStatus: status,
         }),
+        mode: "cors"
+      });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const getAllStudentCourseProgrammeIntakesAPI = async (token: string, pageSize: number, page: number, query: string): Promise<Response | undefined> => {
+  try {
+    return await fetch("http://localhost:8080/api/v1/users/students/course/programme/intake?" + (query ? `query=${query}&` : '') + `pageSize=${pageSize}&page=${page}`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        mode: "cors"
+      });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const getStudentCourseProgrammeIntakeByStudentIdAPI = async (token: string, studentId: number): Promise<Response | undefined> => {
+  try {
+    return await fetch(`http://localhost:8080/api/v1/users/students/${studentId}/course/programme/intake`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        mode: "cors"
+      });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const createStudentCourseProgrammeIntakeAPI = async (token: string, studentId: number, courseId: number, programmeIntakeId: number): Promise<Response | undefined> => {
+  try {
+    return await fetch(`http://localhost:8080/api/v1/users/students/course/programme/intake`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          studentId,
+          courseId,
+          programmeIntakeId,
+        }),
+        mode: "cors"
+      });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const deleteStudentCourseProgrammeIntakeByStudentIdAndCourseIdAndProgrammeIntakeIdAPI = async (token: string, studentId: number, courseId: number, programmeIntakeId: number): Promise<Response | undefined> => {
+  try {
+    return await fetch(`http://localhost:8080/api/v1/users/students/${studentId}/course/${courseId}/programme/intake/${programmeIntakeId}`,
+      {
+        method: 'DELETE',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
         mode: "cors"
       });
   } catch (err) {
