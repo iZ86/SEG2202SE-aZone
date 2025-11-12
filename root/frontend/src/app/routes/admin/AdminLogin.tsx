03558409b1c7e0fd4ced6e8f877cf30dbd0c494d
iZ86 <iZLikesCS@gmail.com>
2025-11-12 22:08:35 +0800

    Fix adminLoginForm not having error 500

--- root/frontend/src/app/routes/admin/AdminLogin.tsx
+++ root/frontend/src/app/routes/admin/AdminLogin.tsx
@@ -1,12 +1,17 @@
 import { AdminLoginForm } from "@features/auth/components/LoginForm";
+import { AdminError500Panel } from "@components/admin/AdminErrorComponent";
+import { useState } from "react";
 
 
 /** TBD: A042 */
 export default function AdminLogin() {
+  const [error500, setError500] = useState(false);
   return (
-    <div className="flex flex-col min-h-screen bg-white-antiflash font-poppins my-auto justify-center items-center">
-      <AdminLoginForm />
+    <div className="flex min-h-screen bg-white-antiflash font-poppins justify-center items-center">
+      
+      {error500 ? <AdminError500Panel /> :
+        <AdminLoginForm setError500={setError500} />
+      }
     </div>
   );
 }
-
