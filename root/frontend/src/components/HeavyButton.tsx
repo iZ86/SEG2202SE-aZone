71b02a2d2131c737847cf1cfe746faf904425f70
skyfoojs <skyfoojs@gmail.com>
2025-11-24 22:00:10 +0800

    Add gap-x-2 to styles and format code

--- root/frontend/src/components/HeavyButton.tsx
+++ root/frontend/src/components/HeavyButton.tsx
@@ -1,15 +1,43 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import type { LucideIcon } from "lucide-react";
-
 
 /** Heavy button format. */
 export default function HeavyButton({
-  buttonText, Icon = undefined, submit = false, backgroundColor = "bg-blue-air-superiority", hoverBgColor = "hover:bg-blue-yinmn", borderColor = "", textColor = "text-white", link = "", hasPadding = true, onClick = undefined
+  buttonText,
+  Icon = undefined,
+  submit = false,
+  backgroundColor = "bg-blue-air-superiority",
+  hoverBgColor = "hover:bg-blue-yinmn",
+  borderColor = "",
+  textColor = "text-white",
+  link = "",
+  hasPadding = true,
+  onClick = undefined,
 }: {
-  buttonText: string, Icon?: LucideIcon, submit?: boolean, backgroundColor?: string, hoverBgColor?: string, borderColor?: string, textColor?: string, link?: string, hasPadding?: boolean, onClick?: () => {}
+  buttonText: string;
+  Icon?: LucideIcon;
+  submit?: boolean;
+  backgroundColor?: string;
+  hoverBgColor?: string;
+  borderColor?: string;
+  textColor?: string;
+  link?: string;
+  hasPadding?: boolean;
+  onClick?: undefined;
 }) {
-
-  const commonClasses = `font-nunito-sans ${Icon ? (hasPadding ? "pl-6 pr-8" : undefined) : (hasPadding ? "px-8" : undefined)} ${hasPadding ? "py-4" : undefined} ${backgroundColor} ${textColor} font-bold text-2xl flex justify-center items-center rounded-lg ${hoverBgColor} ${borderColor.length === 0 ? "" : "border-2 " + borderColor} cursor-pointer`
+  const commonClasses = `font-nunito-sans ${
+    Icon
+      ? hasPadding
+        ? "pl-6 pr-8"
+        : undefined
+      : hasPadding
+      ? "px-8"
+      : undefined
+  } ${
+    hasPadding ? "py-4" : undefined
+  } ${backgroundColor} ${textColor} font-bold text-2xl flex gap-x-2 justify-center items-center rounded-lg ${hoverBgColor} ${
+    borderColor.length === 0 ? "" : "border-2 " + borderColor
+  } cursor-pointer`;
 
   if (link.length > 0) {
     return (
@@ -20,12 +48,14 @@ export default function HeavyButton({
     );
   } else {
     return (
-      <button type={`${submit ? "submit" : "button"}`} className={commonClasses} onClick={onClick}>
+      <button
+        type={`${submit ? "submit" : "button"}`}
+        className={commonClasses}
+        onClick={onClick}
+      >
         {Icon ? <Icon /> : undefined}
         <span>{buttonText}</span>
       </button>
     );
-
   }
-
 }
--- root/frontend/src/components/MediumButton.tsx
+++ root/frontend/src/components/MediumButton.tsx
@@ -1,14 +1,43 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import type { LucideIcon } from "lucide-react";
 
 /** Medium button format. */
 export default function MediumButton({
-  buttonText, Icon = undefined, submit = false, backgroundColor = "bg-blue-air-superiority", hoverBgColor = "hover:bg-blue-yinmn", borderColor = "", textColor = "text-white", link = "", hasPadding = true, onClick = undefined
+  buttonText,
+  Icon = undefined,
+  submit = false,
+  backgroundColor = "bg-blue-air-superiority",
+  hoverBgColor = "hover:bg-blue-yinmn",
+  borderColor = "",
+  textColor = "text-white",
+  link = "",
+  hasPadding = true,
+  onClick = undefined,
 }: {
-  buttonText: string, Icon?: LucideIcon, submit?: boolean, backgroundColor?: string, hoverBgColor?: string, borderColor?: string, textColor?: string, link?: string, hasPadding?: boolean, onClick?: () => void
+  buttonText: string;
+  Icon?: LucideIcon;
+  submit?: boolean;
+  backgroundColor?: string;
+  hoverBgColor?: string;
+  borderColor?: string;
+  textColor?: string;
+  link?: string;
+  hasPadding?: boolean;
+  onClick?: () => void;
 }) {
-
-  const commonClasses = `font-nunito-sans ${Icon ? (hasPadding ? "pl-4 pr-6" : undefined) : (hasPadding ? "px-6" : undefined)} ${hasPadding ? "py-3" : undefined} ${backgroundColor} ${textColor} font-bold text-xl flex gap-x-2 justify-center items-center rounded-md ${hoverBgColor} ${borderColor.length === 0 ? "" : "border-2 " + borderColor} cursor-pointer`
+  const commonClasses = `font-nunito-sans ${
+    Icon
+      ? hasPadding
+        ? "pl-4 pr-6"
+        : undefined
+      : hasPadding
+      ? "px-6"
+      : undefined
+  } ${
+    hasPadding ? "py-3" : undefined
+  } ${backgroundColor} ${textColor} font-bold text-xl flex gap-x-2 justify-center items-center rounded-md ${hoverBgColor} ${
+    borderColor.length === 0 ? "" : "border-2 " + borderColor
+  } cursor-pointer`;
 
   if (link.length > 0) {
     return (
@@ -19,12 +48,14 @@ export default function MediumButton({
     );
   } else {
     return (
-      <button type={`${submit ? "submit" : "button"}`} className={commonClasses} onClick={onClick}>
+      <button
+        type={`${submit ? "submit" : "button"}`}
+        className={commonClasses}
+        onClick={onClick}
+      >
         {Icon ? <Icon /> : undefined}
         <span>{buttonText}</span>
       </button>
     );
-
   }
-
 }
