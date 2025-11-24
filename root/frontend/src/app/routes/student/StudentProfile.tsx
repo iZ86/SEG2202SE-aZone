ca5d19a3411313455683845650a925a7e6b45a3d
skyfoojs <skyfoojs@gmail.com>
2025-11-24 21:30:28 +0800

    Fix minor bug

--- root/frontend/src/app/routes/student/StudentProfile.tsx
+++ root/frontend/src/app/routes/student/StudentProfile.tsx
@@ -1,13 +1,15 @@
 import StudentNavbar from "@components/student/StudentNavbar";
-import ProfileForm from "@features/auth/components/ProfileForm";
+import ProfileForm from "@features/student/components/ProfileForm";
 
 export default function StudentProfile() {
   return (
     <div className="flex flex-col min-h-screen bg-white-antiflash">
-      <StudentNavbar page="dashboard" />
+      <StudentNavbar page="" />
       <main className="px-6 py-6 flex-1">
-        <h1 className="text-3xl font-bold text-slate-900">Edit Student Profile</h1>
-        <ProfileForm userRole="Student" />
+        <h1 className="text-3xl font-bold text-slate-900">
+          Edit Your Profile
+        </h1>
+        <ProfileForm />
       </main>
     </div>
   );
