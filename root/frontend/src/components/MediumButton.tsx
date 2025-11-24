d9883c32f2f26f2383050957c25da142ecc8550d
iZ86 <iZLikesCS@gmail.com>
2025-11-24 17:43:25 +0800

    Modify button to use button tag and add icons to it

--- root/frontend/src/components/MediumButton.tsx
+++ root/frontend/src/components/MediumButton.tsx
@@ -1,22 +1,28 @@
 import { Link } from 'react-router-dom';
-
+import type { LucideIcon } from "lucide-react";
 
 /** Medium button format. */
 export default function MediumButton({
-  buttonText, submit = false, backgroundColor = "bg-blue-air-superiority", hoverBgColor = "hover:bg-blue-yinmn", borderColor = "", textColor = "text-white", link = "", onClick = undefined
+  buttonText, Icon = undefined, submit = false, backgroundColor = "bg-blue-air-superiority", hoverBgColor = "hover:bg-blue-yinmn", borderColor = "", textColor = "text-white", link = "", onClick = undefined
 }: {
-  buttonText: string, submit?: boolean, backgroundColor?: string, hoverBgColor?: string, borderColor?: string, textColor?: string, link?: string, onClick?: () => void
+  buttonText: string, Icon?: LucideIcon, submit?: boolean, backgroundColor?: string, hoverBgColor?: string, borderColor?: string, textColor?: string, link?: string, onClick?: () => void
 }) {
 
-  const commonClasses = `font-nunito-sans px-6 py-3 ${backgroundColor} ${textColor} font-bold text-xl flex justify-center items-center rounded-md ${hoverBgColor} ${borderColor.length === 0 ? "" : "border-2 " + borderColor} cursor-pointer`
+  const commonClasses = `font-nunito-sans ${Icon ? "pl-4 pr-6" : "px-6"} "py-3 ${backgroundColor} ${textColor} font-bold text-xl flex gap-x-2 justify-center items-center rounded-md ${hoverBgColor} ${borderColor.length === 0 ? "" : "border-2 " + borderColor} cursor-pointer`
 
   if (link.length > 0) {
     return (
-      <Link to={link} className={commonClasses}>{buttonText}</Link>
+      <Link to={link} className={commonClasses}>
+        {Icon ? <Icon /> : undefined}
+        <span>{buttonText}</span>
+      </Link>
     );
   } else {
     return (
-      <input type={`${submit ? "submit" : "button"}`} className={commonClasses} value={buttonText} onClick={onClick} />
+      <button type={`${submit ? "submit" : "button"}`} className={commonClasses} onClick={onClick}>
+        {Icon ? <Icon /> : undefined}
+        <span>{buttonText}</span>
+      </button>
     );
 
   }
--- root/frontend/src/components/SmallButton.tsx
+++ root/frontend/src/components/SmallButton.tsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
+import type { LucideIcon } from "lucide-react";
 
 /** Small button format. */
 export default function SmallButton({
   buttonText,
+  Icon = undefined,
   submit = false,
   backgroundColor = "bg-blue-air-superiority",
   hoverBgColor = "hover:bg-blue-yinmn",
@@ -12,6 +14,7 @@ export default function SmallButton({
   onClick = undefined,
 }: {
   buttonText: string;
+  Icon?: LucideIcon;
   submit?: boolean;
   backgroundColor?: string;
   hoverBgColor?: string;
@@ -20,24 +23,22 @@ export default function SmallButton({
   link?: string;
   onClick?: () => void;
 }) {
-  const commonClasses = `font-nunito-sans px-4 py-2 ${backgroundColor} ${textColor} font-bold text-base flex justify-center items-center rounded-sm ${hoverBgColor} ${
-    borderColor.length === 0 ? "" : "border-2 " + borderColor
-  } cursor-pointer`;
+  const commonClasses = `font-nunito-sans ${Icon ? "pl-2 pr-4" : "px-4"} py-2 ${backgroundColor} ${textColor} font-bold text-base flex justify-center items-center rounded-sm ${hoverBgColor} ${borderColor.length === 0 ? "" : "border-2 " + borderColor
+    } cursor-pointer`;
 
   if (link.length > 0) {
     return (
       <Link to={link} className={commonClasses}>
-        {buttonText}
+        {Icon ? <Icon /> : undefined}
+        <span>{buttonText}</span>
       </Link>
     );
   } else {
     return (
-      <input
-        type={`${submit ? "submit" : "button"}`}
-        className={commonClasses}
-        value={buttonText}
-        onClick={onClick}
-      />
+      <button type={`${submit ? "submit" : "button"}`} className={commonClasses} onClick={onClick}>
+        {Icon ? <Icon /> : undefined}
+        <span>{buttonText}</span>
+      </button>
     );
   }
 }
