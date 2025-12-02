5947b619bee459fdccc3611dadbc9698e64ec95f
skyfoojs <skyfoojs@gmail.com>
2025-12-02 10:44:58 +0800

    Update function naming typo

--- root/frontend/src/app/routes/student/StudentSubjectListing.tsx
+++ root/frontend/src/app/routes/student/StudentSubjectListing.tsx
@@ -2,10 +2,10 @@ import StudentNavbar from "@components/student/StudentNavbar";
 import StudentSubjectListingTable from "@features/student/components/SubjectListingTable";
 import StudentInformationPanel from "@features/student/components/ProfileCard";
 
-export default function StudentProgrammeHistory() {
     return (
         <div className="flex flex-col min-h-screen bg-white-antiflash">
             <StudentNavbar page="programmes" />
+export default function StudentSubjectListing() {
 
             <main className="px-10 py-10 flex-1">
                 <h1 className="text-black font-bold">Subject Listing</h1>
