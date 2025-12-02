6cd502da8e93ed156bca42cbd219bf8ebd1da310
skyfoojs <skyfoojs@gmail.com>
2025-12-02 10:50:16 +0800

    Add more semesters
    - Maybe can fetch actual student's semester from db

--- root/frontend/src/features/student/components/SemesterFilterBar.tsx
+++ root/frontend/src/features/student/components/SemesterFilterBar.tsx
@@ -39,6 +39,11 @@ export default function SubjectFilterBar({
           <option value="2">Semester 2</option>
           <option value="3">Semester 3</option>
           <option value="4">Semester 4</option>
+          <option value="5">Semester 5</option>
+          <option value="6">Semester 6</option>
+          <option value="7">Semester 7</option>
+          <option value="8">Semester 8</option>
+          <option value="9">Semester 9</option>
         </select>
       </div>
     </div>
