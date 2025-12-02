10b9dbc23bf9b1c548b44e74d1cceafaf6f888d3
skyfoojs <skyfoojs@gmail.com>
2025-12-02 10:45:49 +0800

    Add getAllStudentEnrollmentSubjectById API

---
+++ root/frontend/src/features/student/api/student.ts
@@ -0,0 +1,15 @@
+export const getAllStudentEnrollmentSubjectByIdAPI = async (token: string, semester: number, pageSize: number, page: number, query: string): Promise<Response | undefined> => {
+  try {
+    return await fetch("http://localhost:8080/api/v1/users/students/enrollment/subject?" + (query ? `query=${query}&` : '') + `pageSize=${pageSize}&page=${page}&semester=${semester}`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        mode: "cors"
+      });
+  } catch (err) {
+    console.error(err);
+  }
+};
