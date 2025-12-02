4efc05e9bd1ae833ac9660e5f399baed20d00ea1
skyfoojs <skyfoojs@gmail.com>
2025-12-02 10:50:22 +0800

    Update indent

--- root/frontend/src/app/routes/student/StudentSubjectListing.tsx
+++ root/frontend/src/app/routes/student/StudentSubjectListing.tsx
@@ -3,18 +3,18 @@ import StudentSubjectListingTable from "@features/student/components/SubjectList
 import StudentProfileCard from "@features/student/components/ProfileCard";
 
 export default function StudentSubjectListing() {
-    return (
-        <div className="flex flex-col min-h-screen bg-white-antiflash">
-            <StudentNavbar page="subject-listing" />
+  return (
+    <div className="flex flex-col min-h-screen bg-white-antiflash">
+      <StudentNavbar page="subject-listing" />
 
       <main className="px-10 py-10 flex-1">
         <h1 className="text-black font-bold">Subject Listing</h1>
 
-                <div className="mt-4">
-                    <StudentProfileCard />
-                </div>
-                <StudentSubjectListingTable />
-            </main>
+        <div className="mt-4">
+          <StudentProfileCard />
         </div>
-    );
+        <StudentSubjectListingTable />
+      </main>
+    </div>
+  );
 }
