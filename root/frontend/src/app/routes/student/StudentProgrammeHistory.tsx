5149448e3e25d7c0051eefad01c4f7048d1b79b7
skyfoojs <skyfoojs@gmail.com>
2025-12-02 08:05:10 +0800

    Update component names and route

--- root/frontend/src/app/routes/student/StudentProgrammeHistory.tsx
+++ root/frontend/src/app/routes/student/StudentProgrammeHistory.tsx
@@ -1,14 +1,14 @@
 import StudentNavbar from "@components/student/StudentNavbar";
-import StudentProgrammeTable from "@features/student/components/ProgrammeHistoryTable";
+import StudentProgrammeHistoryTable from "@features/student/components/ProgrammeHistoryTable";
 
 export default function StudentProgrammeHistory() {
     return (
         <div className="flex flex-col min-h-screen bg-white-antiflash">
-            <StudentNavbar page="programmes" />
+            <StudentNavbar page="programme-history" />
 
             <main className="px-10 py-10 flex-1">
                 <h1 className="text-black font-bold">Programme History</h1>
-                <StudentProgrammeTable />
+                <StudentProgrammeHistoryTable />
             </main>
         </div>
     );
