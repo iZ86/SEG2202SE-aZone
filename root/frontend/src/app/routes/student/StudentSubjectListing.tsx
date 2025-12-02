3c59252f44d6e3a8644e71a7ddbeffbd768c19df
i4GS <itsmason33@gmail.com>
2025-12-02 08:50:15 +0800

    change the page to subject listing

--- root/frontend/src/app/routes/student/StudentSubjectListing.tsx
+++ root/frontend/src/app/routes/student/StudentSubjectListing.tsx
@@ -5,7 +5,7 @@ import StudentProfileCard from "@features/student/components/ProfileCard";
 export default function StudentSubjectListing() {
     return (
         <div className="flex flex-col min-h-screen bg-white-antiflash">
-            <StudentNavbar page="programmes" />
+            <StudentNavbar page="subject-listing" />
 
             <main className="px-10 py-10 flex-1">
                 <h1 className="text-black font-bold">Subject Listing</h1>
