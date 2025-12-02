ebca335a523de82c878739e72fd3f203ec7e2aa8
skyfoojs <skyfoojs@gmail.com>
2025-12-02 11:26:48 +0800

    Update ProfileCard with db data

--- root/frontend/src/features/student/components/ProfileCard.tsx
+++ root/frontend/src/features/student/components/ProfileCard.tsx
@@ -1,68 +1,112 @@
-interface StudentProfileCardProps {
-    name: string;
-    studentId: string;
-    icNo: string;
-    programme: string;
-    intake: string;
-    studyMode: string;
-    semester: number;
-    semesterPeriod: string;
-}
+import LoadingOverlay from "@components/LoadingOverlay";
+import { useEffect, useState } from "react";
+import { getStudentInformationByIdAPI } from "../api/student";
+import { useStudent } from "../hooks/useStudent";
+import { toast } from "react-toastify/unstyled";
+import type { StudentInformation } from "@datatypes/userType";
 
-export default function StudentProfileCard({
-    name = "Isaac Yeow Ming",
-    studentId = "23049679",
-    icNo = "010101-14-1234",
-    programme = "Bachelor of Software Engineering (Honours)",
-    intake = "202509",
-    studyMode = "Full Time",
-    semester = 4,
-    semesterPeriod = "22 Sep 2025 - 16 Jan 2026"
-}: Partial<StudentProfileCardProps>) {
-    return (
-        <div className="bg-white border border-gray-300 rounded-lg p-6 mb-4 text-gray-900">
-            <div className="border-b border-gray-200 pb-4 mb-6">
-                <h2 className="text-2xl font-bold uppercase tracking-tight">{name}</h2>
-            </div>
+export default function StudentProfileCard() {
+  const { authToken, student, loading } = useStudent();
+  const [information, setInformation] = useState<StudentInformation | null>(
+    null
+  );
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-y-6 gap-x-8">
+  useEffect(() => {
+    async function getAllStudentInformationById() {
+      const response = await getStudentInformationByIdAPI(authToken);
 
-                <div>
-                    <span className="block text-xs font-semibold text-gray-500 uppercase mb-1">Student ID</span>
-                    <span className="block font-medium">{studentId}</span>
-                </div>
+      if (!response || !response.ok) {
+        setInformation(null);
+        toast.error("Failed to fetch student information");
+        return;
+      }
 
-                <div>
-                    <span className="block text-xs font-semibold text-gray-500 uppercase mb-1">IC / Passport No</span>
-                    <span className="block font-medium">{icNo}</span>
-                </div>
+      const { data } = await response.json();
 
-                <div>
-                    <span className="block text-xs font-semibold text-gray-500 uppercase mb-1">Study Mode</span>
-                    <span className="block font-medium">{studyMode}</span>
-                </div>
+      if (!data) {
+        setInformation(null);
+        return;
+      }
 
-                <div>
-                    <span className="block text-xs font-semibold text-gray-500 uppercase mb-1">Intake</span>
-                    <span className="block font-medium">{intake}</span>
-                </div>
+      setInformation(data);
+    }
 
-                <div className="md:col-span-2">
-                    <span className="block text-xs font-semibold text-gray-500 uppercase mb-1">Programme</span>
-                    <span className="block font-medium">{programme}</span>
-                </div>
+    if (!student) return;
 
-                <div>
-                    <span className="block text-xs font-semibold text-gray-500 uppercase mb-1">Current Semester</span>
-                    <span className="block font-medium">Semester {semester}</span>
-                </div>
+    getAllStudentInformationById();
+  }, [student, authToken]);
 
-                <div>
-                    <span className="block text-xs font-semibold text-gray-500 uppercase mb-1">Semester Period</span>
-                    <span className="block font-medium">{semesterPeriod}</span>
-                </div>
+  if (loading || !student) {
+    return <LoadingOverlay />;
+  }
+  return (
+    <div className="bg-white border border-gray-300 rounded-lg p-6 mb-4 text-gray-900">
+      <div className="border-b border-gray-200 pb-4 mb-6">
+        <h2 className="text-2xl font-bold uppercase tracking-tight">
+          {student.lastName + " " + student.firstName}'s Profile
+        </h2>
+      </div>
 
-            </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-y-6 gap-x-8">
+        <div>
+          <span className="block text-xs font-semibold text-gray-500 uppercase mb-1">
+            Student ID
+          </span>
+          <span className="block font-medium">{student.userId}</span>
         </div>
-    );
+
+        <div>
+          <span className="block text-xs font-semibold text-gray-500 uppercase mb-1">
+            Email
+          </span>
+          <span className="block font-medium">{student.email}</span>
+        </div>
+
+        <div>
+          <span className="block text-xs font-semibold text-gray-500 uppercase mb-1">
+            Study Mode
+          </span>
+          <span className="block font-medium">{information?.studyMode}</span>
+        </div>
+
+        <div>
+          <span className="block text-xs font-semibold text-gray-500 uppercase mb-1">
+            Intake
+          </span>
+          <span className="block font-medium">{information?.intake}</span>
+        </div>
+
+        <div className="md:col-span-2">
+          <span className="block text-xs font-semibold text-gray-500 uppercase mb-1">
+            Course
+          </span>
+          <span className="block font-medium">{information?.courseName}</span>
+        </div>
+
+        <div>
+          <span className="block text-xs font-semibold text-gray-500 uppercase mb-1">
+            Current Semester
+          </span>
+          <span className="block font-medium">
+            Semester {information?.semester}
+          </span>
+        </div>
+
+        <div>
+          <span className="block text-xs font-semibold text-gray-500 uppercase mb-1">
+            Semester Period
+          </span>
+          <span className="block font-medium">
+            {new Date(
+              information?.semesterStartDate as Date
+            ).toLocaleDateString()}{" "}
+            -{" "}
+            {new Date(
+              information?.semesterEndDate as Date
+            ).toLocaleDateString()}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
