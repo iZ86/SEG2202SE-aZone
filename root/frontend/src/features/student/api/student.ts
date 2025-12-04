f04bcfe4bd619df7afb3d04b40b0c8222e00af7a
iZ86 <iZLikesCS@gmail.com>
2025-12-05 01:53:37 +0800

    Modify students/enrollment/subjects to students/subjects api

--- root/frontend/src/features/student/api/student.ts
+++ root/frontend/src/features/student/api/student.ts
@@ -1,6 +1,6 @@
 export const getAllStudentEnrollmentSubjectByIdAPI = async (token: string, semester: number, pageSize: number, page: number, query: string): Promise<Response | undefined> => {
   try {
-    return await fetch("http://localhost:8080/api/v1/users/students/enrollment/subject?" + (query ? `query=${query}&` : '') + `pageSize=${pageSize}&page=${page}&semester=${semester}`,
+    return await fetch("http://localhost:8080/api/v1/users/students/subjects?" + (query ? `query=${query}&` : '') + `pageSize=${pageSize}&page=${page}&semester=${semester}`,
       {
         method: 'GET',
         headers: {
