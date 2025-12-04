5b5ff0ba8a39fbf06a13e8d0fb234837ef511810
skyfoojs <skyfoojs@gmail.com>
2025-12-04 10:47:48 +0800

    Add methods for programme intake APIs

--- root/frontend/src/features/admin/api/programmes.ts
+++ root/frontend/src/features/admin/api/programmes.ts
@@ -1,3 +1,7 @@
+import {
+  type CalendarDateTime,
+} from "@internationalized/date";
+
 export const getAllProgrammesAPI = async (token: string, pageSize?: number, page?: number, query?: string): Promise<Response | undefined> => {
   try {
     return await fetch("http://localhost:8080/api/v1/programmes?" + (query ? `query=${query}&` : '') + `pageSize=${pageSize}&page=${page}`,
@@ -84,9 +88,25 @@ export const deleteProgrammeByIdAPI = async (token: string, programmeId: number)
   }
 };
 
-export const getAllProgrammeIntakesAPI = async (token: string): Promise<Response | undefined> => {
+export const getAllProgrammeIntakesAPI = async (token: string, pageSize?: number, page?: number, query?: string): Promise<Response | undefined> => {
   try {
-    return await fetch("http://localhost:8080/api/v1/programmes/intake",
+    return await fetch("http://localhost:8080/api/v1/programmes/intake?" + (query ? `query=${query}&` : '') + `pageSize=${pageSize}&page=${page}`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        mode: "cors"
+      });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const getProgrammeIntakeByIdAPI = async (token: string, programmeIntakeId: number): Promise<Response | undefined> => {
+  try {
+    return await fetch(`http://localhost:8080/api/v1/programmes/intake/${programmeIntakeId}`,
       {
         method: 'GET',
         headers: {
@@ -105,6 +125,70 @@ export const getProgrammeIntakesByProgrammeIdAPI = async (token: string, program
     return await fetch(`http://localhost:8080/api/v1/programmes/${programmeId}/intake`,
       {
         method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        mode: "cors"
+      });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const createProgrammeIntakeAPI = async (token: string, programmeId: number, intakeId: number, studyModeId: number, semester: number, semesterStartDate: CalendarDateTime, semesterEndDate: CalendarDateTime): Promise<Response | undefined> => {
+  try {
+    return await fetch("http://localhost:8080/api/v1/programmes/intake",
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          programmeId,
+          intakeId,
+          studyModeId,
+          semester,
+          semesterStartDate: semesterStartDate.toString().slice(0, 19).replace('T', ' '),
+          semesterEndDate: semesterEndDate.toString().slice(0, 19).replace('T', ' '),
+        }),
+        mode: "cors"
+      });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const updateProgrammeIntakeByIdAPI = async (token: string, programmeIntakeId: number, programmeId: number, intakeId: number, studyModeId: number, semester: number, semesterStartDate: CalendarDateTime, semesterEndDate: CalendarDateTime): Promise<Response | undefined> => {
+  try {
+    return await fetch(`http://localhost:8080/api/v1/programmes/intake/${programmeIntakeId}`,
+      {
+        method: 'PATCH',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          programmeId,
+          intakeId,
+          studyModeId,
+          semester,
+          semesterStartDate: semesterStartDate.toString().slice(0, 19).replace('T', ' '),
+          semesterEndDate: semesterEndDate.toString().slice(0, 19).replace('T', ' '),
+        }),
+        mode: "cors"
+      });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const deleteProgrammeIntakeByIdAPI = async (token: string, programmeIntakeId: number): Promise<Response | undefined> => {
+  try {
+    return await fetch(`http://localhost:8080/api/v1/programmes/intake/${programmeIntakeId}`,
+      {
+        method: 'DELETE',
         headers: {
           Authorization: `Bearer ${token}`,
           'Content-Type': 'application/json'
