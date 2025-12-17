7a903869038ff78effff011445e69bcd75b8a568
iZ86 <iZLikesCS@gmail.com>
2025-12-18 02:25:48 +0800

    Add StudentProfileCard to StudentProgrammeHistory

--- root/frontend/src/app/routes/student/StudentProgrammeHistory.tsx
+++ root/frontend/src/app/routes/student/StudentProgrammeHistory.tsx
@@ -1,15 +1,19 @@
 import StudentNavbar from "@components/student/StudentNavbar";
+import StudentProfileCard from "@features/student/components/ProfileCard";
 import StudentProgrammeHistoryTable from "@features/student/components/ProgrammeHistoryTable";
 
 export default function StudentProgrammeHistory() {
-    return (
-        <div className="flex flex-col min-h-screen bg-white-antiflash">
-            <StudentNavbar page="programme-history" />
+  return (
+    <div className="flex flex-col min-h-screen bg-white-antiflash">
+      <StudentNavbar page="programme-history" />
 
-            <main className="px-10 py-10 flex-1">
-                <h1 className="text-black font-bold">Programme History</h1>
-                <StudentProgrammeHistoryTable />
-            </main>
+      <main className="px-10 py-10 flex-1">
+        <h1 className="text-black font-bold">Programme History</h1>
+        <div className="mt-4">
+          <StudentProfileCard />
         </div>
-    );
+        <StudentProgrammeHistoryTable />
+      </main>
+    </div>
+  );
 }
