4cc9bbd1626cbb1f81bfbfa959e4b5f3ac6475ab
SkyFooJS <skyfoojs@gmail.com>
2026-02-05 16:26:05 +0800

    feat(lecturer): add param validator

--- root/backend/app/routes/lecturer.route.ts
+++ root/backend/app/routes/lecturer.route.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { asyncHandler } from "../utils/utils";
 import LecturerController from "../controllers/lecturer.controller";
 import { checkAuthTokenHeader, verifyAdminAuthToken, verifyAuthToken, verifyAuthTokenHeader } from "../middlewares/auth";
-import { createAndUpdateLecturerValidator } from "../validators/lecturer-validator";
+import { createAndUpdateLecturerValidator, lecturerParamValidator, lecturerTitleParamValidator } from "../validators/lecturer-validator";
 
 class LecturerRoute {
   router = Router();
@@ -15,14 +15,14 @@ class LecturerRoute {
   initializeRoutes() {
     this.router.get("/", checkAuthTokenHeader, verifyAuthTokenHeader, verifyAdminAuthToken, verifyAuthToken, asyncHandler(this.controller.getLecturers));
     this.router.get("/titles", checkAuthTokenHeader, verifyAuthTokenHeader, verifyAdminAuthToken, verifyAuthToken, asyncHandler(this.controller.getLecturerTitles));
-    this.router.get("/titles/:lecturerTitleId", checkAuthTokenHeader, verifyAuthTokenHeader, verifyAdminAuthToken, verifyAuthToken, asyncHandler(this.controller.getLecturerTitleById));
-    this.router.get("/:lecturerId", checkAuthTokenHeader, verifyAuthTokenHeader, verifyAdminAuthToken, verifyAuthToken, asyncHandler(this.controller.getLecturerById));
+    this.router.get("/titles/:lecturerTitleId", checkAuthTokenHeader, verifyAuthTokenHeader, verifyAdminAuthToken, verifyAuthToken, lecturerTitleParamValidator, asyncHandler(this.controller.getLecturerTitleById));
+    this.router.get("/:lecturerId", checkAuthTokenHeader, verifyAuthTokenHeader, verifyAdminAuthToken, verifyAuthToken, lecturerParamValidator, asyncHandler(this.controller.getLecturerById));
 
     this.router.post("/", checkAuthTokenHeader, verifyAuthTokenHeader, verifyAdminAuthToken, verifyAuthToken, createAndUpdateLecturerValidator, asyncHandler(this.controller.createLecturer));
 
-    this.router.put("/:lecturerId", checkAuthTokenHeader, verifyAuthTokenHeader, verifyAdminAuthToken, verifyAuthToken, createAndUpdateLecturerValidator, asyncHandler(this.controller.updateLecturerById));
+    this.router.put("/:lecturerId", checkAuthTokenHeader, verifyAuthTokenHeader, verifyAdminAuthToken, verifyAuthToken, lecturerParamValidator, createAndUpdateLecturerValidator, asyncHandler(this.controller.updateLecturerById));
 
-    this.router.delete("/:lecturerId", checkAuthTokenHeader, verifyAuthTokenHeader, verifyAdminAuthToken, verifyAuthToken, asyncHandler(this.controller.deleteLecturerById));
+    this.router.delete("/:lecturerId", checkAuthTokenHeader, verifyAuthTokenHeader, verifyAdminAuthToken, verifyAuthToken, lecturerParamValidator, asyncHandler(this.controller.deleteLecturerById));
   }
 }
 
