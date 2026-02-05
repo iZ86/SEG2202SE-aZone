48f66e567cdeadd7537e3bd44a23f64deb3af812
SkyFooJS <skyfoojs@gmail.com>
2026-02-05 15:46:33 +0800

    feat(venue): add param validator

--- root/backend/app/routes/venue.route.ts
+++ root/backend/app/routes/venue.route.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { asyncHandler } from "../utils/utils";
 import VenueController from "../controllers/venue.controller";
 import { checkAuthTokenHeader, verifyAdminAuthToken, verifyAuthToken, verifyAuthTokenHeader, verifyStudentAuthToken } from "../middlewares/auth";
-import { createAndUpdateVenueValidator } from "../validators/venue-validator";
+import { createAndUpdateVenueValidator, venueParamValidator } from "../validators/venue-validator";
 
 class VenueRoute {
   router = Router();
@@ -15,13 +15,13 @@ class VenueRoute {
   initializeRoutes() {
 
     this.router.get("/", checkAuthTokenHeader, verifyAuthTokenHeader, verifyAdminAuthToken, verifyAuthToken, asyncHandler(this.controller.getVenues));
-    this.router.get("/:venueId", checkAuthTokenHeader, verifyAuthTokenHeader, verifyAdminAuthToken, verifyAuthToken, asyncHandler(this.controller.getVenueById));
+    this.router.get("/:venueId", checkAuthTokenHeader, verifyAuthTokenHeader, verifyAdminAuthToken, verifyAuthToken, venueParamValidator, asyncHandler(this.controller.getVenueById));
 
     this.router.post("/", checkAuthTokenHeader, verifyAuthTokenHeader, verifyAdminAuthToken, verifyAuthToken, createAndUpdateVenueValidator, asyncHandler(this.controller.createVenue));
 
-    this.router.put("/:venueId", checkAuthTokenHeader, verifyAuthTokenHeader, verifyAdminAuthToken, verifyAuthToken, createAndUpdateVenueValidator, asyncHandler(this.controller.updateVenueById));
+    this.router.put("/:venueId", checkAuthTokenHeader, verifyAuthTokenHeader, verifyAdminAuthToken, verifyAuthToken, venueParamValidator, createAndUpdateVenueValidator, asyncHandler(this.controller.updateVenueById));
 
-    this.router.delete("/:venueId", checkAuthTokenHeader, verifyAuthTokenHeader, verifyAdminAuthToken, verifyAuthToken, asyncHandler(this.controller.deleteVenueById));
+    this.router.delete("/:venueId", checkAuthTokenHeader, verifyAuthTokenHeader, verifyAdminAuthToken, verifyAuthToken, venueParamValidator, asyncHandler(this.controller.deleteVenueById));
   }
 }
 
--- root/backend/app/validators/venue-validator.ts
+++ root/backend/app/validators/venue-validator.ts
@@ -1,5 +1,13 @@
-import { body } from "express-validator";
+import { body, param } from "express-validator";
 import validate from "../middlewares/validate";
+
+export const venueParamValidator: any = [
+  param('venueId')
+    .exists().withMessage("Invalid venueId")
+    .isInt().withMessage("Invalid venueId")
+    .toInt(),
+  validate,
+];
 
 export const createAndUpdateVenueValidator: any = [
   body('venue')
