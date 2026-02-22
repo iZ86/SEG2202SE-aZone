43cd9dcecc80a1f2f164d3f21d7862c80740263f
iZ86 <iZLikesCS@gmail.com>
2026-02-22 21:54:35 +0800

    Add datatype converter to createAndUpdateSubjectValidator

--- root/backend/app/validators/subject-validator.ts
+++ root/backend/app/validators/subject-validator.ts
@@ -5,23 +5,28 @@ export const createAndUpdateSubjectValidator: any = [
   body('subjectName')
     .trim()
     .notEmpty().withMessage('Missing subjectName')
-    .isString().withMessage('subjectName must be a string'),
+    .isString().withMessage('subjectName must be a string')
+    .toString(),
   body('subjectCode')
     .trim()
     .notEmpty().withMessage('Missing subjectCode')
-    .isString().withMessage('subjectCode must be a string'),
+    .isString().withMessage('subjectCode must be a string')
+    .toString(),
   body('description')
     .trim()
     .optional()
     .isString().withMessage('description must be string')
-    .isLength({ max: 300 }).withMessage('description cannot exceed 300 characters'),
+    .isLength({ max: 300 }).withMessage('description cannot exceed 300 characters')
+    .toString(),
   body('creditHours')
     .trim()
     .notEmpty().withMessage('Missing creditHours')
-    .isNumeric().withMessage('creditHours must be a number'),
+    .isNumeric().withMessage('creditHours must be a number')
+    .toInt(),
   body('courseIds')
     .isArray({ min: 1 }).withMessage('courseIds must be an array and at least one value'),
   body('courseIds.*')
-    .isNumeric().withMessage('All items must be a number'),
+    .isNumeric().withMessage('All items must be a number')
+    .toInt(),
   validate,
 ];
