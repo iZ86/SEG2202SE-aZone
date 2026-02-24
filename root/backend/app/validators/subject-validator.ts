18639ec527d9b3517ff39ec53459936e6a1d6e32
iZ86 <iZLikesCS@gmail.com>
2026-02-24 21:31:21 +0800

    Remove .toString in createAndUpdateSubjectValidator causing nodejs crash
    because .toString is a native method, and there is also no need to
    convert to string as its already in stringi

--- root/backend/app/validators/subject-validator.ts
+++ root/backend/app/validators/subject-validator.ts
@@ -5,19 +5,16 @@ export const createAndUpdateSubjectValidator: any = [
   body('subjectName')
     .trim()
     .notEmpty().withMessage('Missing subjectName')
-    .isString().withMessage('subjectName must be a string')
-    .toString(),
+    .isString().withMessage('subjectName must be a string'),
   body('subjectCode')
     .trim()
     .notEmpty().withMessage('Missing subjectCode')
-    .isString().withMessage('subjectCode must be a string')
-    .toString(),
+    .isString().withMessage('subjectCode must be a string'),
   body('description')
     .trim()
     .optional()
     .isString().withMessage('description must be string')
-    .isLength({ max: 300 }).withMessage('description cannot exceed 300 characters')
-    .toString(),
+    .isLength({ max: 300 }).withMessage('description cannot exceed 300 characters'),
   body('creditHours')
     .trim()
     .notEmpty().withMessage('Missing creditHours')
