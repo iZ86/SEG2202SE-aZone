a0a5f8bee9fe4b91d0daaaa39b85801d45381e5e
iZ86 <iZLikesCS@gmail.com>
2026-03-05 19:30:08 +0800

    Add IntakeWithCountData model

--- root/backend/app/models/intake-model.ts
+++ root/backend/app/models/intake-model.ts
@@ -3,3 +3,8 @@ import { RowDataPacket } from "mysql2";
 export interface IntakeData extends RowDataPacket {
   intakeId: number;
 }
+
+export interface IntakeWithCountData {
+  intakes: IntakeData[];
+  intakeCount: number;
+}
