2ecdb169ce65d7a2b01532b69393ab3b74609541
iZ86 <iZLikesCS@gmail.com>
2026-03-06 19:09:11 +0800

    Add ProgrammeIntakeParamValidator to getProgrammeIntakeById

--- root/backend/app/controllers/programme.controller.ts
+++ root/backend/app/controllers/programme.controller.ts
@@ -125,11 +125,7 @@ export default class ProgrammeController {
   }
 
   async getProgrammeIntakeById(req: Request, res: Response) {
-    const programmeIntakeId: number = parseInt(req.params.programmeIntakeId as string);
-
-    if (!programmeIntakeId || isNaN(programmeIntakeId)) {
-      return res.sendError.badRequest("Invalid programmeIntakeId");
-    }
+    const programmeIntakeId: number = Number(req.params.programmeIntakeId);
 
     const response: Result<ProgrammeIntakeData> = await programmeService.getProgrammeIntakeById(programmeIntakeId);
 
