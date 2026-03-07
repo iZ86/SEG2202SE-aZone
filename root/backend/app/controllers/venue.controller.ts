e6cc13dec0fb3bf5c7b751a62da6a34fe559c0da
iZ86 <iZLikesCS@gmail.com>
2026-03-08 00:47:45 +0800

    Renive controller venue as number conversion for...
    deleteVenueById

--- root/backend/app/controllers/venue.controller.ts
+++ root/backend/app/controllers/venue.controller.ts
@@ -73,7 +73,7 @@ export default class VenueController {
   }
 
   async deleteVenueById(req: Request, res: Response) {
-    const venueId: number = Number(req.params.venueId as string);
+    const venueId: number = Number(req.params.venueId);
 
     const response = await venueService.deleteVenueById(venueId);
 
