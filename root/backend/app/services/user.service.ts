dfe82cf712bb04b284bb0b1b2b8d7bea7723159c
iZ86 <iZLikesCS@gmail.com>
2026-03-18 20:59:04 +0800

    Fix service updateUserProfilePictureById check..
    ing userResult rather than isSuccess() state

--- root/backend/app/services/user.service.ts
+++ root/backend/app/services/user.service.ts
@@ -211,8 +211,8 @@ class UserService implements IUserService {
   public async updateUserProfilePictureById(userId: number, profilePictureUrl: string): Promise<Result<UserData>> {
     const userResult: Result<UserData> = await this.getUserById(userId);
 
-    if (!userResult) {
       return Result.fail(ENUM_ERROR_CODE.ENTITY_NOT_FOUND, "Invalid userId");
+    if (!userResult.isSuccess()) {
     }
 
     const updateUserProfilePictureResult: ResultSetHeader = await userRepository.updateUserProfilePictureById(userId, profilePictureUrl);
