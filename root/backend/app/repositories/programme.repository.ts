9dce59a1f512906aead6b2f1b10878bfe4e4fa28
iZ86 <iZLikesCS@gmail.com>
2026-03-21 23:17:04 +0800

    Modify repo parameter status getProgrammeIntakes..
    ByStatus from status to programmeIntakeStatusId

--- root/backend/app/repositories/programme.repository.ts
+++ root/backend/app/repositories/programme.repository.ts
@@ -31,7 +31,7 @@ interface IProgrammeRepository {
   createStudentCourseProgrammeIntake(studentId: number, courseId: number, programmeIntakeId: number, studentCourseProgrammeIntakeStatusId: number): Promise<ResultSetHeader>;
   deleteStudentCourseProgrammeIntake(studentId: number, courseId: number, programmeIntakeId: number): Promise<ResultSetHeader>;
   getProgrammeDistribution(): Promise<ProgrammeDistribution[]>;
-  getProgrammeIntakesByStatus(status: number): Promise<ProgrammeIntakeData[]>;
+  getProgrammeIntakesByStatus(programmeIntakeStatusId: number): Promise<ProgrammeIntakeData[]>;
   isExistProgrammeIntakesByProgrammeId(programmeId: number): Promise<IsExist | undefined>;
 }
 
@@ -476,7 +476,7 @@ class ProgrammeRepository implements IProgrammeRepository {
     });
   }
 
-  public getProgrammeIntakesByStatus(status: number): Promise<ProgrammeIntakeData[]> {
+  public getProgrammeIntakesByStatus(programmeIntakeStatusId: number): Promise<ProgrammeIntakeData[]> {
     return new Promise((resolve, reject) => {
       databaseConn.query<ProgrammeIntakeData[]>(
         "SELECT pi.programmeIntakeId, pi.programmeId, p.programmeName, pi.intakeId, pi.studyModeId, sm.studyMode, pi.semester, pi.semesterStartDate, pi.semesterEndDate, pi.enrollmentId, pi.programmeIntakeStatusId " +
@@ -486,7 +486,7 @@ class ProgrammeRepository implements IProgrammeRepository {
         "INNER JOIN STUDY_MODE sm ON pi.studyModeId = sm.studyModeId " +
         "WHERE pi.programmeIntakeStatusId = ?;",
         [
-          status
+          programmeIntakeStatusId
         ],
         (err, res) => {
           if (err) reject(err);
