ff6bd09b63a43406f0dd3d2a4b5b5754eb5e1e63
iZ86 <iZLikesCS@gmail.com>
2026-03-28 01:47:06 +0800

    Add service getProgrammeIntakesByProgrammeIntake..
    StatusId studyMode to programmeIntakes

--- root/backend/app/services/programme.service.ts
+++ root/backend/app/services/programme.service.ts
@@ -648,6 +648,12 @@ class ProgrammeService implements IProgrammeService {
     const programmeIntakes: ProgrammeIntakeData[] = await programmeRepository.getProgrammeIntakesByProgrammeIntakeStatusId(programmeIntakeStatusId);
 
     for (const programmeIntake of programmeIntakes) {
+      const studyMode = ENUM_STUDY_MODE_ID[programmeIntake.studyModeId];
+      programmeIntake.studyMode = studyMode
+        .split('_')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+        .join(' ');
+
       const programmeIntakeStatus = ENUM_PROGRAMME_INTAKE_STATUS_ID[programmeIntake.programmeIntakeStatusId]
       programmeIntake.programmeIntakeStatus = programmeIntakeStatus.charAt(0) + programmeIntakeStatus.slice(1).toLowerCase();
     }
