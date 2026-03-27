a8dbf90d7fd3265809d866c1e3d95110a1d0f990
iZ86 <iZLikesCS@gmail.com>
2026-03-28 01:41:30 +0800

    Modify repo getProgrammeIntakeById sql query...
    to add placeholder for studyMode column and remove STUDY_MODE table join

--- root/backend/app/repositories/programme.repository.ts
+++ root/backend/app/repositories/programme.repository.ts
@@ -198,11 +198,10 @@ class ProgrammeRepository implements IProgrammeRepository {
   public getProgrammeIntakeById(programmeIntakeId: number): Promise<ProgrammeIntakeData | undefined> {
     return new Promise((resolve, reject) => {
       databaseConn.query<ProgrammeIntakeData[]>(
-        "SELECT pi.programmeIntakeId, pi.programmeId, p.programmeName, pi.intakeId, pi.studyModeId, sm.studyMode, pi.semester, pi.semesterStartDate, pi.semesterEndDate, pi.enrollmentId, pi.programmeIntakeStatusId " +
+        "SELECT pi.programmeIntakeId, pi.programmeId, p.programmeName, pi.intakeId, pi.studyModeId, '' AS studyMode, pi.semester, pi.semesterStartDate, pi.semesterEndDate, pi.enrollmentId, pi.programmeIntakeStatusId " +
         "FROM PROGRAMME_INTAKE pi " +
         "INNER JOIN PROGRAMME p ON pi.programmeId = p.programmeId " +
         "INNER JOIN INTAKE i ON pi.intakeId = i.intakeId " +
-        "INNER JOIN STUDY_MODE sm ON pi.studyModeId = sm.studyModeId " +
         "WHERE pi.programmeIntakeId = ?;",
         [programmeIntakeId],
         (err, res) => {
