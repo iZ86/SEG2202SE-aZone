869f337724599dc0a86debc6ab2d61670923f051
iZ86 <iZLikesCS@gmail.com>
2026-07-06 00:50:05 +0800

    Add navbarItem Type

---
+++ root/frontend/src/datatypes/navItemType.ts
@@ -0,0 +1,8 @@
+
+export type NavbarItem = {
+  dashboard: { label: string, to: string },
+  profile: { label: string, to: string },
+  settings: { label: string, to: string },
+  signOut: { label: string, to: string },
+}
+
